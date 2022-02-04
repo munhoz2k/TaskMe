3b91770ff567fc9e73e130926d80162fc54bdc8d
HeitorFranco <heitorfranco04@gmail.com>
2022-02-04 06:22:41 -0300

    retornando o token quando o usuário é criado

--- src/api/controllers/createUser.ts
+++ src/api/controllers/createUser.ts
@@ -2,14 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { CreateUser } from "../services/CreateUser";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
+  const data = req.body;
+  try {
+    const { user, token } = await CreateUser.execute(data);
 
-    const data = req.body
-    try {
-        const newUser = await CreateUser.execute(data)
-
-        res.status(201).json(newUser)
-    } catch (error) {
-        next(error)
-    }
-    
-}
+    res.status(201).json({ user, token });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/api/services/CreateUser.ts
+++ src/api/services/CreateUser.ts
@@ -1,31 +1,47 @@
-import { PrismaClient, User } from "@prisma/client"
-import bcrypt from "bcrypt"
+import { PrismaClient, User } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
 
-const db = new PrismaClient()
+const db = new PrismaClient();
 
 interface Request {
-    name:  string
-    email: string
-    password: string
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
 }
 
 export class CreateUser {
-
-    static async execute(data: Request): Promise<User> {
-
-        // Checks if email is being used
-        const verifyEmail = await db.user.findFirst({ where: { email: data.email } }) 
-        if(verifyEmail) {
-            throw new Error('This email is being used')
-        }
-
-        // Encrypt password
-        data.password = await bcrypt.hash(data.password, 10)
-
-        // Create user on Database
-        const newUser = await db.user.create({ data: data })
-
-        return newUser
+  static async execute(data: Request): Promise<Response> {
+    // Checks if email is being used
+    const verifyEmail = await db.user.findFirst({
+      where: { email: data.email },
+    });
+    if (verifyEmail) {
+      throw new Error("This email is being used");
     }
 
+    // Encrypt password
+    data.password = await bcrypt.hash(data.password, 10);
+
+    // Create user on Database
+    const newUser = await db.user.create({ data: data });
+
+    // Creating JWT
+    const payload = {
+      id: newUser.id,
+    };
+    const token = sign(payload, process.env.SERVER_PASSWORD, {
+      expiresIn: "30d",
+    });
+
+    // Deleting users pass for security prevention
+    delete newUser.password;
+
+    return { user: newUser, token };
+  }
 }
