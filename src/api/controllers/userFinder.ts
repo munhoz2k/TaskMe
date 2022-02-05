7e5aef0457bf18f48cfdd6a5dc770752617f7a95
HeitorFranco <heitorfranco04@gmail.com>
2022-02-05 08:49:55 -0300

    removendo funções desnecessárias(file: userFinder.ts)

--- src/api/controllers/userFinder.ts
+++ src/api/controllers/userFinder.ts
@@ -1,46 +1,16 @@
-import { NextFunction, Request, Response } from "express";
-import { FindUser } from "../services/FindUser";
+import { NextFunction, Request, Response } from "express"
+import { FindUser } from "../services/FindUser"
 
 export const userFinder = {
-  all: async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const users = await FindUser.findAll();
+    byToken: async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const user = await FindUser.findById(req.user.id)
 
-      res.status(200).json(users);
-    } catch (error) {
-      next(error);
-    }
-  },
+            user.photo_path = `${process.env.API_URL}/uploads/${user.photo_path}`
 
-  byToken: async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const user = await FindUser.findById(req.user.id);
-
-      res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  },
-
-  byId: async (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
-    try {
-      const users = await FindUser.findById(id);
-
-      res.status(200).json(users);
-    } catch (error) {
-      next(error);
-    }
-  },
-
-  byEmail: async (req: Request, res: Response, next: NextFunction) => {
-    const { email } = req.body;
-    try {
-      const users = await FindUser.findById(email);
-
-      res.status(200).json(users);
-    } catch (error) {
-      next(error);
-    }
-  },
-};
+            res.status(200).json(user)
+        } catch (error) {
+            next(error)
+        }
+    },
+}
