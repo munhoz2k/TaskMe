c56d7f1e14fd1b662db9f58937d5812137251b05
munhoz2k <lucasmunhozarruda@gmail.com>
2022-03-08 12:25:37 -0300

    Servidor de Tasks

--- src/api/server.ts
+++ src/api/server.ts
@@ -4,7 +4,7 @@ import { app } from "./app"
 import { router } from "./routes"
 import cors from "cors"
 
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 app.use(cors())
 app.use(router)
